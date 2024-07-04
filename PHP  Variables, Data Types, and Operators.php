d21a8861e42a1bba75de27cd4ed509d901e9aeb8
Salih <sskarakussalih77@gmail.com>
2024-07-04 17:57:35 +0300

    Variables

---
+++ PHP  Variables, Data Types, and Operators.php
@@ -0,0 +1,37 @@
+<?php
+
+echo phpversion();
+
+$vary = "PHP";
+echo "my variable is vary <br>" ;
+//ayni sey
+echo ("my variable is vary <br>") ;
+print "selamlar";
+// bu bir yorum satiridir.
+/* bu da coklu yorum satiridir. */
+# bu da yorum satiri yapmanin baska yoludur.
+
+//degisken tanimlama
+$age = "19";
+$AGE = "20";
+$name = "salih";
+
+$x = $y = $z = "yalova";
+
+echo "<h3> su anda $age yasindayim ve 2 ay sonra $AGE yasinda olacagim </h3>";
+echo "yasim" . $age . "!" ;
+
+//degisken toplama 
+echo $age + $AGE;
+echo $name + $age;
+
+//degisken turu dondurme
+var_dump($age);
+var_dump($name);
+
+
+
+
+
+
+?>
