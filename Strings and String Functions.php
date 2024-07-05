65a43db7f860fb62c0115aae099f9ac7f48ca49e
Salih <sskarakussalih77@gmail.com>
2024-07-05 15:29:35 +0300

    Dizeler ve Dize Fonksiyonlari

---
+++ Strings and String Functions.php
@@ -0,0 +1,17 @@
+<?php
+
+$dizem = "karakus";
+
+//dize elemenlarinin harflerini buyutme
+echo strtoupper($dizem);
+
+//dize elemanlarini harflerini kucultme
+echo strtolower($dizem);
+
+//dizeyi ters sekilde yazma
+echo strrev($dizem);
+
+//dizeler ile ilgili bircok ekstra bilgiyi data types dosyasinda bulabilirsiniz
+//bu kodlarin tamami salih karakus tarafindan yazilmistir.
+
+?>
