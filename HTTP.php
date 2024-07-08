cd5958ab657c1669af7705a5334de2f392c14f42
Salih <sskarakussalih77@gmail.com>
2024-07-08 11:48:16 +0300

    isim degisiklikleri

---
+++ HTTP.php
@@ -0,0 +1,15 @@
+<?php
+$buttonName = "GONDER";
+?>
+
+<html>
+    <head></head>
+    <body>
+        <form action="">
+            <input type="text">
+            <input type="text" name="" id="">
+            <input type="submit" value="<?php echo $buttonName;?>">
+        </form>
+    </body>
+</html>
+
