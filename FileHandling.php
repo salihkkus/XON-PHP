b83708155308842160eba978cb28095aa9c62e02
Salih <sskarakussalih77@gmail.com>
2024-07-08 18:34:15 +0300

    .

--- FileHandling.php
+++ FileHandling.php
@@ -5,7 +5,7 @@ $dosyam = fopen("phpdosyam.txt" , "w") or die ("Dosyaya Erişilemiyor.");
 $txt = "Benim ismim Salih <br>";
 fwrite($dosyam , $txt);
 
-$txt = "Şu anda XON şirketınde stajyer olarak çalışmaktayım";
+$txt = "Şu anda XON şirketinde stajyer olarak çalışmaktayım <br>";
 fwrite($dosyam , $txt);
 
 fread($dosyam , filesize("phpdosyam.txt"));
