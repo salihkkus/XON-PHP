1de96ba08aa2bc01e31c7c8a5fdcc63f6ba2b6f7
Salih <sskarakussalih77@gmail.com>
2024-07-08 14:13:41 +0300

    form

---
+++ HTTP.php
@@ -0,0 +1,29 @@
+<?php
+
+// Form İşleme ve Doğrulama: Form işleme ve doğrulamayı gösteren bir PHP betiği oluşturun:
+
+?>
+
+<html>
+<body>
+
+
+<form>
+
+<label for ="fname">İsim:</label><br>
+<input type ="text" id = fname name =fname value = Salih><br>
+
+<label for = "lname">Soyisim:<label><br>
+<input type = "text" id = lname name = lname value = Karakuş><br>
+
+<label for ="eposta">E-Posta</label><br>
+<input type="text" id = eposta name = eposta value = sskarakussalih77@gmail.com><br>
+
+<label for = "onay">Onayla:</label><br>
+<input type="submit" name = onay value="Onayla">
+
+</form>
+
+
+</body>
+</html>
