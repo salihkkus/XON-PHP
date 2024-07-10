5b5ed5e5e5c14d8e7fcf45d343159e84cdd08b52
Salih <sskarakussalih77@gmail.com>
2024-07-10 11:31:04 +0300

    .

--- MySQL/DataInsert.php
+++ MySQL/DataInsert.php
@@ -1,5 +1,12 @@
 <?php
-
+echo "asd";
+error_reporting(E_ALL);
+/*
+INSERT KAYIT EKLEME
+DELETE KAYIT SILME
+SELECT KAYIT SECME LISTLEME 
+UPDATE KAYIT GUNCELLEME
+*/
 //$mysqli = new mysqli("sunucu", "kullanici", "sifre", "database");
 
 /* ILK YONTEM
@@ -22,12 +29,46 @@ $mysqli = mysqli_connect("localhost", "root", "", "company");
 
 if(mysqli_connect_errno())
 {
-    echo "fail";
+    echo "Bağlantı Başarısız <br>";
 }
 
 else 
 {
-    echo "success";
+    echo "Bağlantı Başarılı <br>";
 }
 
+if($_POST)
+$e_fname = $_POST["fname"];
+$e_lname = $_POST["lname"];
+$e_mail = $_POST["mail"];
+$e_salary = $_POST["salary"];
+
+if($mysqli)
+$sql = "insert into employee (first_name,last_name,email, salary) values('" .$e_id. "','" .$e_fname. "','" .$e_lname. "','" .$e_mail. "','" .$e_salary. "')";
+
+
 ?>
+
+<form action="DataInsert.php" method="post">
+<html>
+<body>
+
+<label for="fname">Çalışan Adı</label>
+<input type="text" id="fname" name="fname">
+<br>
+<label for="lname">Çalışan Soyadı</label>
+<input type="text" id="lname" name="lname">
+<br>
+<label for="mail">Çalışan Email</label>
+<input type="text" id="mail" name="mail">
+<br>
+<label for="salary">Çalışan Maaşı</label>
+<input type="text" id="salary" name="salary">
+<br>
+<label for="onay">Kaydet</label>
+<input type="submit" id="onay" name="onay">
+<br>
+
+</body>
+</html>
+</form>
